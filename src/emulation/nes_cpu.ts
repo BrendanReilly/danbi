6eb63ead05b8436a9b2e61f954ac7197c160c816
Brendan Reilly <brendan@breilly.io>
2021-05-15 22:33:21 -0400

    Updated opcode definitions Updated index

--- src/emulation/nes_cpu.ts
+++ src/emulation/nes_cpu.ts
@@ -1,3 +1,5 @@
+import { OpCode, opCodes } from "./opcodes";
+
 export class NesCpu {
 
     /**
@@ -5,13 +7,41 @@ export class NesCpu {
      */
     private _statusRegister: u8;
 
+    private _xRegister: u8;
+
+    private _yRegister: u8;
+
     /**
      * Opcode lookup table
      */
-    private _opcodes: { [opcode: number]: (args: i8[]) => void };
+    private _opCodes: { [opcode: number]: OpCode };
+
+    public get xRegister(): u8 {
+        return this._xRegister;
+    }
+
+    public set xRegister(val: u8) {
+        this._xRegister = val;
+    }
+
+    public get yRegister(): u8 {
+        return this._yRegister;
+    }
+
+    public set yRegister(val: u8) {
+        this._yRegister = val;
+    }
+
+    public get statusRegister(): u8 {
+        return this._statusRegister;
+    }
+
+    public set statusRegister(val: u8) {
+        this._statusRegister = val;
+    }
 
     public constructor() {
-        this._opcodes = {};
+        this._opCodes = opCodes;
         this._statusRegister = 0;
     }
 
@@ -19,10 +49,21 @@ export class NesCpu {
      * Executes the passed opcode with  the passed arguments.
      * @param opcode 
      */
-    public execute(opcode: i8, ...args: []): void {
-        this._opcodes[opcode](args);
+    public execute(opcode: u8, ...args: (u8[] | i8[])): void {
+        // execute and update status register changes
+        switch (args.length) {
+            case 0:
+                this._opCodes[opcode](this);
+                break;
+            case 1:
+                this._opCodes[opcode](this, args[0]);
+                break;
+            case 2:
+                this._opCodes[opcode](this, args[0], args[1]);
+                break;
+            default:
+                break;
+        }
     }
-
-
 
 }
--- src/emulation/opcodes.ts
+++ src/emulation/opcodes.ts
@@ -1,3 +1,9 @@
-export const  opcodes = {
+import { NesCpu } from "./nes_cpu";
+export type OpCode = (cpu: NesCpu, ...args: number[]) => void;
+export const opCodes: { [opCode: number]: OpCode } = {};
 
+// CLC
+opCodes[0x18] = (cpu: NesCpu): void => {
+    cpu.statusRegister |= 0b10000000;
+    return;
 }
